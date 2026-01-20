926df5355d4c8c146b14cfb514b9718ff9f923e5
ultron37 <sreenandan131@gmail.com>
2026-01-20 22:42:57 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,14 +1,1 @@
-import os
-import json
-import queue
-import requests
-import sounddevice as sd
-from vosk import Model, KaldiRecognizer
 
-VOSK_MODEL_PATH = "vosk-model-small-en-us-0.15"
-OLLAMA_URL = "http://localhost:11434/api/generate"
-OLLAMA_MODEL = "phi3"
-
-def speak(text):
-    print("AI:",text)
-    os.system(f'espeak-ng"{text}"')
